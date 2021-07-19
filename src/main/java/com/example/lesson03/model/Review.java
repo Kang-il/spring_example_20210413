38248d00745bf8f41b45a624c838a5efd79c56fd
NOSICKK\nosickk <h5tchi@naver.com>
2021-07-19 15:03:14 +0900

    Kang-il/spring_example_20210413#2 20210716 Mybatis Example 01

---
+++ src/main/java/com/example/lesson03/model/Review.java
@@ -0,0 +1,61 @@
+package com.example.lesson03.model;
+
+import java.util.Date;
+
+public class Review {
+
+	private Integer id;
+	private String storeName;
+	private String menu;
+	private double point;
+	private String review;
+	private Date createdAt;
+	private Date updatedAt;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getStoreName() {
+		return storeName;
+	}
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+	public String getMenu() {
+		return menu;
+	}
+	public void setMenu(String menu) {
+		this.menu = menu;
+	}
+	public double getPoint() {
+		return point;
+	}
+	public void setPoint(double point) {
+		this.point = point;
+	}
+	public String getReview() {
+		return review;
+	}
+	public void setReview(String review) {
+		this.review = review;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
+	
+	
+}
